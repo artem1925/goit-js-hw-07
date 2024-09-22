765941d707577378e61d4ca5dc9f687c9ee63ba3
artem1925 <157005541+artem1925@users.noreply.github.com>
2024-09-22 14:30:04 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// Отримуємо посилання на елементи
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+// Додаємо обробник події click на кнопку
+changeColorButton.addEventListener('click', () => {
+  // Генеруємо випадковий колір
+  const randomColor = getRandomHexColor();
+  
+  // Змінюємо колір фону body
+  document.body.style.backgroundColor = randomColor;
+  
+  // Встановлюємо значення кольору в span
+  colorSpan.textContent = randomColor;
+});
